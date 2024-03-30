86b1839ac82373f1a72093db9d8b9a4744dc0680
chamithKavinda <chamith13kavinda@gmail.com>
2024-03-30 14:27:45 +0530

    basic js

---
+++ js/lesson-js.js
@@ -0,0 +1,17 @@
+//console.log("Hello Universal!!")
+
+// console.log(10+20)
+
+// console.log(50-20)
+
+// console.log(50/5)
+
+// console.log(10*7)
+
+console.log("Add : 30")
+
+console.log("Sub : ")
+
+console.log("Devide : ")
+
+console.log("Multiple : ")
