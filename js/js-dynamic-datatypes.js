d60fe12b02d069808701ae7e19326c561eef5ae8
chamithKavinda <chamith13kavinda@gmail.com>
2024-03-30 15:48:39 +0530

    dynamic-datatypes

---
+++ js/js-dynamic-datatypes.js
@@ -0,0 +1,8 @@
+let testingVariable;
+console.log("Type of testingVariable : " , typeof testingVariable)
+
+testingVariable = "Chamith"
+console.log("Type of testingVariable : " , typeof testingVariable)
+
+testingVariable = 658
+console.log("Type of testingVariable : " , typeof testingVariable)
