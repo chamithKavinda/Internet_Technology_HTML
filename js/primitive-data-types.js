10e686b2c0eeccdb2addabb1eabc34ce2502c9e4
chamithKavinda <chamith13kavinda@gmail.com>
2024-03-30 15:32:00 +0530

    primitive data types in js

---
+++ js/primitive-data-types.js
@@ -0,0 +1,29 @@
+//there are 6 types in javascript
+
+//1.String
+let firstName = "chamith" 
+console.log("Types of firstName : " , typeof firstName )
+
+//2.Number
+let age = 30;
+console.log("Types of Age : " , typeof age)
+
+let distance = 30;
+console.log("Types of distance : " , typeof distance)
+
+//3.Boolean
+let isDeleted = false
+console.log("Types of isDeleted : " , typeof isDeleted)
+
+//4.UnderFined
+let address;
+console.log("Types of address : " , typeof address)
+
+let email = undefined;
+console.log("Types of email : " , typeof email)
+
+//5.Null
+let phoneNumber = null
+console.log("Types of phoneNumber : " , typeof phoneNumber)
+
+//6.Symbol - Introduced in EOMAScript 6(ES6), symbols are unique and imutable primitive value
