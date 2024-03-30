a3792591e4b19070c0e99e942aac92fa0c93b879
chamithKavinda <chamith13kavinda@gmail.com>
2024-03-30 14:57:28 +0530

    js variables content

---
+++ js/js-variables.js
@@ -0,0 +1,19 @@
+//String firstName = "Kamal";
+//var , let , const
+
+let firstName = "Chmaith"
+let SecondName = "Kavinda"
+
+console.log(firstName , SecondName)
+
+var age = 30;
+//var salary = 45.25;
+
+const salary = 50000.00
+
+//Important
+//1. can not be a reserved word.
+//2. variable name should be meaningful
+//3. can not start with numbers
+//4.can not contain spaces or hypenents
+//5.case sensitive
