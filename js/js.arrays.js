fd5180a205817ed36897aca4b685508a251d26c9
chamithKavinda <chamith13kavinda@gmail.com>
2024-04-02 21:52:31 +0530

    js arrays update

---
+++ js/js.arrays.js
@@ -0,0 +1,22 @@
+let fruits = []
+console.log("Types of fruits : " , typeof fruits)
+
+fruits = ['apple' , 'banana']
+console.log("Fruits array : " , fruits)
+console.log("Types array length : " , fruits.length)
+
+console.log("Fruits 1 : " , fruits[0])
+console.log("Fruits 2 : " , fruits[4])
+
+
+fruits[5] = 'avacado'
+console.log("fruits 6 : ", fruits[5])
+console.log("Fruits array : " , fruits)
+
+fruits[4] = 'avacado'
+console.log("fruits 6 : ", fruits[5])
+console.log("Fruits array : " , fruits)
+
+
+
+
