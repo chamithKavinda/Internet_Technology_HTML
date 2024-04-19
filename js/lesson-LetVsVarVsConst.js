83a8a5162dfa8944825ddd76647e7947d0926187
chamithKavinda <chamith13kavinda@gmail.com>
2024-04-19 16:46:13 +0530

    let vs var update

--- js/lesson-LetVsVarVsConst.js
+++ js/lesson-LetVsVarVsConst.js
@@ -1,13 +1,13 @@
 function iterateNumbers() {
     for (var i = 0; i < 5; i++) {
-        console.log(i);
+      console.log(i);
     }
+  
+    // console.log(i)
+  }
+  
+  iterateNumbers();
+  
+  //var -> function-scoped
+  //let,const -> block-scoped  this comes from js6
 
-   // console.log(i)
-}
-
-iterateNumbers();
-
-
-//var -> function-scoped
-//let,const -> block-scoped
