abb0c0353908f6bcec5f07c77438427728ac045e
chamithKavinda <chamith13kavinda@gmail.com>
2024-04-20 15:23:37 +0530

    js constructor

---
+++ js/Constructor.js
@@ -0,0 +1,8 @@
+function Person(name,age,email){
+    this.name=name;
+    this.age=age;
+    this.email=email;
+}
+
+let person = new Person("Dasun" , 25 , 'chamith132@gamil.com');
+console.log(person)
