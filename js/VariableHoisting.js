cd45b45486a65485efc689a746ec601297431373
chamithKavinda <chamith13kavinda@gmail.com>
2024-04-20 14:26:49 +0530

    Variable Hoisting done

---
+++ js/VariableHoisting.js
@@ -0,0 +1,17 @@
+var number= undefined
+console.log(number)
+var number = 100
+
+// with var,variable declarations are hoisted to the top of their containing functions or global scope which means you can  access the variable before it is declared without casting and error although its value will be underfined until itsassgned.
+
+//console.log(firstName)
+//let firstName = 'Kamal'
+
+//========== variable hoisting with let keyword =============
+//console.log(FirstName)
+//let firstName = "kamal"
+
+//======== variable hoisting with const keyword ===============
+
+// console.log(lastName)
+// const lastName = "perera"
