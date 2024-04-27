b6ae8c617d031a8a20aa26b7ff2d039adbbf349d
chamithKavinda <chamith13kavinda@gmail.com>
2024-04-27 12:26:36 +0530

    js element selectors update

--- js/lesson-js-element-selection.js
+++ js/lesson-js-element-selection.js
@@ -20,3 +20,19 @@ console.log(boxes)
 //tag Selector
 let headingOnes = document.getElementsByTagName('h1')
 console.log(headingOnes)
+
+//querySelector
+let button = document.querySelector('btnCalculate')
+console.log(button)
+
+let testDiv = document.querySelector('-test-div')
+console.log(testDiv)
+
+let box = document.querySelector('.box')
+console.log(box)
+
+let hb4 = document.querySelectorAll('.box')
+console.log(hb4)
+
+let bb = document.querySelectorAll('#container .box')
+console.log(bb)
