7cd953a0ffd1391e4d5aaa0f9d86af29d2ab5d6b
chamithKavinda <chamith13kavinda@gmail.com>
2024-04-27 11:35:53 +0530

    js-Timer-Excersice 2 done

--- Excersices/Excersice-Timer.js
+++ Excersices/Excersice-Timer.js
@@ -1,10 +1,23 @@
-let count = 1;
+// let count = 1;
+
+// const intervalId = setInterval(() => {
+//   if (count <= 10) {
+//     console.log(count);
+//     count++;
+//   } else {
+//     clearInterval(intervalId); 
+//   }
+
+// }, 2000);
 
 const intervalId = setInterval(() => {
-  if (count <= 10) {
-    console.log(count);
-    count++;
-  } else {
-    clearInterval(intervalId); 
-  }
-}, 100);
+
+      console.log('ABCD');
+
+    }, 1000);
+
+console.log('ABCD')
+
+setTimeout(()=>{
+    clearInterval(intervalId)
+},5000)
